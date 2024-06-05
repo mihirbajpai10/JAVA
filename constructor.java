8494b79c609f08e47d9c93e68833a269917e36e0
mihirbajpai10 <mihir.bajpai10@gmail.com>
2024-06-05 19:20:56 +0530

    Constructor

---
+++ constructor.java
@@ -0,0 +1,33 @@
+class Test
+{
+    /* Default Constructor */
+    // int i;
+    // String s;
+    // public static void main(String[] args)
+    // {
+    //     Test t = new Test();
+    //     System.out.println(t.i+" "+t.s);
+    // }
+
+
+    /* no-arg Constructor */
+    // Test()
+    // {
+    //     System.out.println("no arg constructor");
+    // }
+    // public static void main(String[] args)
+    // {
+    //     Test t = new Test();
+    // }
+
+
+    /* Parameterized Constructor */
+        // Test(int a)
+        // {
+        //     System.out.println("Parameterized constructor");
+        // }
+        // public static void main(String[] args)
+        // {
+        //     Test t = new Test(10);
+        // }
+}
