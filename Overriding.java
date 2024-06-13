f67e3a8706febb63db99ef2f8e5045b06cb92bfe
mihirbajpai10 <mihir.bajpai10@gmail.com>
2024-06-13 23:36:31 +0530

    Polymorphism

--- Overriding.java
+++ Overriding.java
@@ -1,3 +1,28 @@
+// class Test
+// {
+//     void show()
+//     {
+//         System.out.println("No arguments");
+//     }
+// }
+// class Marks extends Test
+// {
+//     void show()
+//     {
+//         System.out.println("One argument: " + 10);
+//     }
+//     public static void main(String[] args) {
+//         Test t = new Test();
+//         Marks m = new Marks();
+//         t.show();
+//         m.show();
+//     }
+// }
+
+
+/* Invorking overridden method from sub-class*/
+
+
 class Test
 {
     void show()
@@ -9,12 +34,13 @@ class Marks extends Test
 {
     void show()
     {
+        super.show();
         System.out.println("One argument: " + 10);
     }
     public static void main(String[] args) {
-        Test t = new Test();
+        
         Marks m = new Marks();
-        t.show();
+        
         m.show();
     }
 }
